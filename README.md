74193a69b18dbd3893f96f08365ac078693f3b89
SPYxWIZ <92371167+SPYxWIZ@users.noreply.github.com>
2023-04-20 03:19:49 +0530

    Add files via upload
    Milestone 3.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_application_2
+# jimm
 
 A new Flutter project.
 
---
+++ android/app/src/main/kotlin/com/example/jimm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jimm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
